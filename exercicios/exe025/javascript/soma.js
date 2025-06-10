944227ebd792fe14674110c89c3a59e92bb68134
Guilherme Freitas <gui2010mg68@gmail.com>
2025-06-09 23:56:20 -0300

    .

---
+++ exercicios/exe025/javascript/soma.js
@@ -0,0 +1,10 @@
+function soma() {
+    let n1 = document.getElementById('in1')
+    let n2 = document.getElementById('in2')
+    let somar = n1 + n2
+
+    return somar
+
+}
+
+isoma.innerHTMl = somar
